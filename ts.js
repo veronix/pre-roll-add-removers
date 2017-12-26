6bd615516e5c6aef7ca4df9cb90050ec87bd08d3
Veronica Scott <veronix@users.noreply.github.com>
2017-12-27 01:07:50 +0600

    initial commit

---
+++ ts.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Pre-roll ad remover for OC.kg
+// @version      0.1
+// @description  Automatically skips pre-roll adds on oc.kg
+// @author       veronix
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @match        http://*.oc.kg/*
+// ==/UserScript==
+
+console.log('add remover script started');
+waitForKeyElements('video',() => {
+    console.log('video ready');
+
+    videojs("player5").ready(function(){
+        var player = this;
+        player.autoplay(true);
+
+        function skip() {
+            var dur = player.duration();
+            console.log("duration of ad: " + dur);
+            player.currentTime(dur);
+        }
+
+        player.on('play', function() {
+            if (player.src().includes('oops')) {
+                console.log('ad == true');
+                skip();
+            }
+            console.log('playing');
+        });
+    });
+
+});
